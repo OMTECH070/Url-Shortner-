99b1f1a780b422bf3d3ef15a7b3adc7626443fd4
Om Mahadik <ommahsdik1a@gmail.com>
2026-08-17 21:32:03 +0530

    Changes

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ import crypto from 'crypto'
 const __filename = fileURLToPath(import.meta.url) 
 const __dirname = path.dirname(__filename) 
  
-const PORT=3000 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+}); 
  
 const DATA_FILE=path.join("data","links.json") 
  
